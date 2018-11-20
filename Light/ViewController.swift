c1b6847bc19f6fb6e3f8c352249c6b594b8c6c73
Hameed Abdullah <hameedabdullah@hameeds-mbp.bmcc.cuny.edu>
2018-11-19 20:26:20 -0500

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Hameed Abdullah on 11/19/18.
+//  Copyright © 2018 Hameed Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
