3f6a0db549c0961c2eef7e89a21e6f80ce2b22df
Hameed Abdullah <hameedabdullah@Hameeds-MacBook-Pro.local>
2018-11-26 23:43:01 -0500

    intial commit

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var lightButton: UIButton!
+    
+    var brightness: CGFloat = 1.0  //1.0 is maximum, 0.0 is minimum
+    
+   var lightOn = true
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
+    
+    @objc func buttonTapped() {
+        print("Button Tapped")
+        view.backgroundColor = .yellow
+    }
+    
+//    @IBAction func onAndOffTapped(_ sender: Any) {
+//    
+//        //lightOn = !lightOn
+//        updateUI()
+//    }
+    
+    
+    @IBAction func swipeDown(_ sender: UISwipeGestureRecognizer) {
+        print("SwipeDown")
+        brightness -= 0.1
+        updateUI()
+    }
+    
+    @IBAction func swipeUp(_ sender: UISwipeGestureRecognizer) {
+        print("SwipeUp")
+        brightness += 0.1
+        updateUI()
+    }
+    
+    func updateUI() {
+        view.backgroundColor = UIColor(red: brightness, green: brightness, blue: brightness, alpha: 1.0)
+        
+//        if lighOn {
+//            view.backgroundColor = .white
+//        } else {
+//            view.backgroundColor = .black
+//        }
+        
+    }
+    
+ 
 
-
+  
+    
+    
 }
 
